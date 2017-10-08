d79665cdbd27aa4ee6c4901cf778940b690c0942
angelmer <maximer@mail.ru>
2017-10-08 21:55:13 +0300

    add script for main-menu and modal

--- js/main.js
+++ js/main.js
@@ -11,14 +11,40 @@ function init() {
     behaviors: ['drag']
   });
   var placemark = new ymaps.Placemark([59.938631, 30.323055], {
-    hintContent: 'Mishka',
-    balloonContent: 'Милые штуки ручной работы',
-  },
-  {
-    iconLayout: 'default#image',
-    iconImageHref: 'img/icon-map-pin.svg',
-    iconImageSize: [46, 57],
-    iconImageOffset: [-30, -65]
-  });
+      hintContent: 'Mishka',
+      balloonContent: 'Милые штуки ручной работы'
+    },
+    {
+      iconLayout: 'default#image',
+      iconImageHref: 'img/icon-map-pin.svg',
+      iconImageSize: [46, 57],
+      iconImageOffset: [-30, -65]
+    });
   map.geoObjects.add(placemark);
 }
+
+var toggler = document.querySelector('.main-menu__toggle');
+var mainMenu = document.querySelector('.main-menu');
+mainMenu.classList.remove('main-menu--nojs');
+toggler.addEventListener('click', function (event) {
+  event.preventDefault();
+  mainMenu.classList.contains('main-menu--closed') ?
+    (mainMenu.classList.add('main-menu--opened'),
+      mainMenu.classList.remove('main-menu--closed')) :
+    (mainMenu.classList.add('main-menu--closed'),
+      mainMenu.classList.remove('main-menu--opened'))
+});
+
+var order = document.querySelector('.best-goods__price-link');
+var modal = document.querySelector('.modal');
+var modalOverlay = document.querySelector('.modal__overlay');
+order.addEventListener('click', function () {
+  event.preventDefault();
+  modal.classList.add('modal--open');
+  modalOverlay.classList.add('modal--open')
+});
+
+modalOverlay.addEventListener('click', function () {
+  modalOverlay.classList.remove('modal--open');
+  modal.classList.remove('modal--open');
+});
